d4f35d7692ede513b203dfd149c462745442f9bd
Jaseibert <Jaseibert5@gmail.com>
2017-08-01 19:02:42 -0500

    Initial Commit

---
+++ ARTest/SpaceShip.swift
@@ -0,0 +1,23 @@
+//
+//  SpaceShip.swift
+//  ARTest
+//
+//  Created by Jeremy Seibert on 6/24/17.
+//  Copyright © 2017 Jeremy Seibert. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class SpaceShip: SCNNode {
+    
+    // Create the SceneKit Model
+    func loadModel() {
+        guard let virtualObjectScene = SCNScene(named:"art.scnassests/ship.sch") else {return}
+        let wrapperNode = SCNNode()
+        for child in virtualObjectScene.rootNode.childNodes {
+            wrapperNode.addChildNode(child)
+        }
+        self.addChildNode(wrapperNode)
+    }
+}
---
+++ ARTest/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ARTest
+//
+//  Created by Jeremy Seibert on 6/24/17.
+//  Copyright © 2017 Jeremy Seibert. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var counterLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = SCNScene()
+        
+        sceneView.scene = scene
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Wire up the ARSession
+        let configuration = ARSessionConfiguration()
+        sceneView.session.run(configuration)
+        addObject()
+        
+    }
+    
+    // Setup & Load the Spaceship Object
+    func addObject() {
+        let ship = SpaceShip()
+        ship.loadModel()
+        
+        //Establish the Random Postion
+        let xPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        let yPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        ship.position = SCNVector3(xPos,yPos, -1)
+        
+        sceneView.scene.rootNode.addChildNode(ship)
+    }
+    
+    
+    func randomPosition(lowerBound lower: Float, upperBound upper: Float) -> Float {
+        return Float(arc4random()) / Float(UInt32.max) * (lower - upper) + upper
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
